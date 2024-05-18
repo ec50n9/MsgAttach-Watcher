19e5d22bdbaa775946d7610942d32ddc7a5b8699
ec50n9 <shelloworld@qq.com>
2024-05-18 10:17:08 +0800

    fix: 导出xlsx文件时，进行非法字符转换

--- utils/excel.py
+++ utils/excel.py
@@ -1,5 +1,6 @@
 from typing import Dict, List
 import openpyxl
+from openpyxl.cell.cell import ILLEGAL_CHARACTERS_RE
 
 def save_dict_to_excel(data: List[Dict[str, str]], filename: str):
   workbook = openpyxl.Workbook()
@@ -9,7 +10,10 @@ def save_dict_to_excel(data: List[Dict[str, str]], filename: str):
   worksheet.append(headers)
 
   for row in data:
-    row = [str(row[header]) for header in headers]
+    row = [fix_illegal_characters(row[header]) for header in headers]
     worksheet.append(row)
 
   workbook.save(filename)
+
+def fix_illegal_characters(text: str) -> str:
+  return ILLEGAL_CHARACTERS_RE.sub('', text)
